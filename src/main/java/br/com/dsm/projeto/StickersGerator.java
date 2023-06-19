e168ed1e88fffea961f3728fa63c933200334eb9
HenriqueDeFraiaPaschoal <110928555+HenriqueDeFraiaPaschoal@users.noreply.github.com>
2023-06-19 10:00:11 -0300

    Update StickersGerator.java
    Added margin left to the generator and added import to work the stringWidth(message)

--- src/main/java/br/com/dsm/projeto/StickersGerator.java
+++ src/main/java/br/com/dsm/projeto/StickersGerator.java
@@ -3,6 +3,7 @@ package br.com.dsm.projeto;
 import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -26,11 +27,16 @@ public class StickersGerator {
             Graphics2D graphics = newImage.createGraphics();
             graphics.drawImage(originalImage, 0, 0, null);
             
-            Font font = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+            Font font = new Font(Font.SANS_SERIF, Font.BOLD, width / 10);
             graphics.setFont(font);
             graphics.setColor(Color.YELLOW);
+
+            FontMetrics fontMetrics = graphics.getFontMetrics();
+            int textWidth = fontMetrics.stringWidth(message);
+            int marginLeft = (width - textWidth) / 2;
+            int marginTop = height + (200 - (width / 10)) / 2;
             
-            graphics.drawString(message, 200, newHeight - 80);
+            graphics.drawString(message, marginLeft, marginTop );
             
             ImageIO.write(newImage, "png", new File("exit/" + fileName));
         } catch (IOException e) {
